b3f526f473e0260d5687692a3d261ddfd45f7aab
Guillaume Fraysse <gfraysse.spam+github@gmail.com>
2019-07-31 15:44:57 +0200

    Update

--- Mutex/Naimi-Trehel/Go/naimi-trehel.go
+++ Mutex/Naimi-Trehel/Go/naimi-trehel.go
@@ -2,89 +2,156 @@
 //    M. Naimi, M. Tréhel, A. Arnold, "A Log(N) Distributed Mutual Exclusion Algorithm Based on the Path Reversal", Journal of Parallel and Distributed Computing, 34, 1-13 (1996).
 //    M.Tréhel, M.Naimi: "Un algorithme distribué d'exclusion mutuelle", TSI Vol 6, no 2, p. 141–150, (1987).
 //    M.Naimi, M. Tréhel : "How to detect a failure and regenerate the token in the Log(n) distributed algorithm for mutual exclusion" , 2nd International Workshop on Distributed Algorithms, Amsterdam, (Juill. 1987), paru dans Lecture Notes in Computer Science, no 312, p. 149-158, édité par J. Van Leeween.
+// https://www-master.ufr-info-p6.jussieu.fr/2018/spip.php?action=acceder_document&arg=23891&cle=5597164cc7d5a16ea0ce06e8b68c2c226bf8de89&file=pdf%2FNaimi_Trehel.pdf
 
 // Complexity O(Log(n))
 package main
 
 import (
 	"fmt"
+	"log"
 	"sync"
 	"strings"
 	"strconv"
 )
 
-func enterCS(id int) {
-	fmt.Println("inCS : goroutine #", id)	
+type Node struct {
+	id         int
+	has_token  bool
+	requesting bool
+	next       int
+	father     int
+	channel    chan string
+	messages   [10]chan string
 }
 
-func Algo(id int, messages [10]chan string, wg *sync.WaitGroup) {
-	// var waiting_list []int 
-	fmt.Println("goroutine #", id)
-	// var str strings.Builder
+func (n *Node) enterCS() {
+	log.Print("goroutine #", n.id, " enterCS")
+}
+
+func (n *Node) requestCS() {
+	// log.Print("goroutine #", n.id, " requestCS")
+	log.Print("goroutine #", n.id, " requestCS, n.father=", n.father)
+	n.requesting = true
+	if n.father != -1 {
+		var content = fmt.Sprintf("REQ%d", n.id)
+		log.Print("goroutine #", n.id, " requestCS, SENDING ", content, " to father #", n.father)				
+		n.messages[n.father] <- content
+		n.father = -1		
+	} else {
+		log.Print("goroutine #", n.id, " father == -1")				
+	}
+}
+
+func (n *Node) releaseCS() {
+	log.Print("goroutine #", n.id, " releaseCS")
+	n.requesting = false
+	if n.next != -1 {
+		var content = fmt.Sprintf("token%d", n.next)
+		log.Print("goroutine #", n.id, " releaseCS, SENDING ", content, " to next #", n.next)				
+		n.messages[n.next] <- content
+		n.has_token = false
+		n.next = -1
+	}
+}
+
+func (n *Node) receiveRequestCS(j int) {
+	log.Print("goroutine #", n.id, " receiveRequestCS")
+	if n.father == -1 {
+		if n.requesting {
+			n.next = j
+		} else {
+			n.has_token = false
+			var content = fmt.Sprintf("token%d", j)
+			log.Print("goroutine #", n.id, " receiveRequestCS SENDING ", content, " to j #", j)				
+			n.messages[j] <- content
+		}		
+	} else {
+		var content = fmt.Sprintf("REQ%d", n.id)
+		log.Print("goroutine #", n.id, " receiveRequestCS SENDING ", content, " to father #", n.father)				
+		n.messages[n.father] <- content
+	}
+	n.father = j
+	log.Print("goroutine #", n.id, " receiveRequestCS, n.father=", n.father)
+
+}
+
+func (n *Node) receiveToken() {
+	log.Print("goroutine #", n.id, " receiveToken")
+	log.Print("****** Got TOKEN #", n.id)
+	n.has_token = true
+}
+
+func (n *Node) NaimiTrehel(wg *sync.WaitGroup) {
+	log.Print("goroutine #", n.id)
 
 	// Initialization
-	var has_token = false
-	// var requested = false
-	var child = -1
-	var father = 1
+	n.has_token = false
+	n.requesting = false
+	n.next = -1
+	n.father = 1
 
-	if father == id {
-		has_token = true
-		father = -1
+	if n.father == n.id {
+		n.has_token = true
+		n.father = -1
 	} else {
-		has_token = false
+		n.has_token = false
 	}
 
-	for i := 1; i < 100; i ++ {
-		if father == -1 {
-			// requested = true
-			enterCS(id)
-			// requested = false
-			if child != -1 {
-				var content = fmt.Sprintf("token%d", child)
-				fmt.Println("SENDING", content)				
-				messages[child] <- content
-				has_token = false
-				child = -1
+	for i := 1; i < 100; i ++ {		
+		if n.father == -1 {
+			if n.has_token {
+				n.enterCS()
+				//if n.next != -1 {
+				n.releaseCS()
+				// } 				
+			} else {
+				log.Print("Is father but does not have the token, WTF ?")
 			}
 		} else {
-			var content = fmt.Sprintf("REQ%d", id)
-			fmt.Println("SENDING ", content, "to father", father)				
-			messages[father] <- content
+			n.requestCS()
 		}
-		msg := <-messages[id]
-		fmt.Println("msg received=", msg)	
+		msg := <-n.messages[n.id]
+		// log.Print("msg received=", msg)	
 		if (strings.Contains(msg, "REQ")) {
 			var requester, err = strconv.Atoi(msg[3:])
 			if err != nil {
-				fmt.Println(err)
+				log.Fatal(err)
 			}
-			fmt.Println("received REQ=", requester, "by goroutine#", id)	
+			// log.Print("received REQ=", requester, "by goroutine#", n.id)
+			n.receiveRequestCS(requester)
+			
 		} else if (strings.Contains(msg, "token")) {
-
+			n.receiveToken()
 		} else {
-			fmt.Println("WTF")	
+			log.Fatal("WTF")	
 		}
 	}
 
-	fmt.Println("has_token", has_token)	
+	log.Print("goroutine #", n.id," has_token=", n.has_token)	
 	wg.Done()
 }
 
 func main() {
-	var nb_process = 10
+	//var nb_process = 10
 	var wg sync.WaitGroup
+	var nodes [10]Node
 	var messages [10]chan string
 	
-	fmt.Println("nb_process #", nb_process)
+	log.Print("nb_process #", len(nodes))
 	
-	for i := 0; i < nb_process; i++ {
+	for i := 0; i < len(nodes); i++ {
+		nodes[i].id = i
+		nodes[i].channel = messages[i]
 		messages[i] = make(chan string)
 	}
+	for i := 0; i < len(nodes); i++ {
+		nodes[i].messages = messages
+	}
 	
-	for i := 0; i < nb_process; i++ {
+	for i := 0; i < len(nodes); i++ {
 		wg.Add(1)
-		go Algo(i, messages, &wg)
+		go nodes[i].NaimiTrehel(&wg)
 	}
 	wg.Wait()
 }
